c1b66c6d9b5c96d269760c1e208d797a4e526c33
Cosmodude <vladislav.lenskii@gmail.com>
2024-12-09 23:21:24 +0900

    script

--- script/Counter.s.sol
+++ script/Counter.s.sol
@@ -1,6 +1,10 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+/** 
+forge script script/Counter.s.sol --broadcast --rpc-url badnet --account DEV \
+--verify --verifier blockscout --verifier-url https://blockscout.badnet.mitosis.org/api/
+**/
 import {Script, console} from "forge-std/Script.sol";
 import {Counter} from "../src/Counter.sol";
 
