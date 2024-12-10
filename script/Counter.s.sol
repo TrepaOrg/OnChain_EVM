3e697435fdd74feb680f9f21a1c5e84603b87a4e
Cosmodude <vladislav.lenskii@gmail.com>
2024-12-10 19:09:10 +0900

    sol version + clean up

--- script/Counter.s.sol
+++ script/Counter.s.sol
@@ -1,17 +1,11 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.20;
 
-/** 
-forge script script/Counter.s.sol --broadcast --rpc-url badnet --account DEV \
---verify --verifier blockscout --verifier-url https://blockscout.badnet.mitosis.org/api/
-**/
 import {Script, console} from "forge-std/Script.sol";
 import {Counter} from "../src/Counter.sol";
 
 contract CounterScript is Script {
     Counter public counter;
-
-    function setUp() public {}
 
     function run() public {
         vm.startBroadcast();
