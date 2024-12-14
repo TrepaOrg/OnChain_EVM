1aae905582e9e951ca03c257fbdf35a63c4dd018
Cosmodude <vladislav.lenskii@gmail.com>
2024-12-14 20:43:36 +0900

    factory instead of proxy

--- script/ProxyFactory.s.sol
+++ script/ProxyFactory.s.sol
@@ -2,17 +2,17 @@
 pragma solidity ^0.8.20;
 
 import {Script, console} from "forge-std/Script.sol";
-import {SafeProxy} from "@gnosis/safe-contracts/proxies/SafeProxy.sol";
+import {SafeProxyFactory} from "@gnosis/safe-contracts/proxies/SafeProxyFactory.sol";
 
 address constant singleton = 0x000000000000000000000000000000000000dEaD;
 contract FactoryScript is Script {
-    SafeProxy public account;
+    SafeProxyFactory public factory;
 
 
     function run() public {
         vm.startBroadcast();
 
-        account = new SafeProxy(singleton);
+        factory = new SafeProxyFactory();
 
         vm.stopBroadcast();
     }
